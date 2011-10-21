54aa92f94b7f9404b0174aabeaa900b845b9f7f5
Colin Powell <colin@onecardinal.com>
2011-10-21 18:28:06 -0300

    Allowing multiple shipping addresses per user, for a specific project.

--- shop/addressmodel/models.py
+++ shop/addressmodel/models.py
@@ -33,7 +33,7 @@ class Country(models.Model):
 
 
 class Address(models.Model):
-    user_shipping = models.OneToOneField(User, related_name='shipping_address',
+    user_shipping = models.ManyToManyField(User, related_name='shipping_address',
                                          blank=True, null=True)
     user_billing = models.OneToOneField(User, related_name='billing_address',
                                         blank=True, null=True)
